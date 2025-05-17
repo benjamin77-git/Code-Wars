687f7b3b517c20711b7859dc9363345da55cec19
Mugisha Benjamin <m.mugishabenjamin@gmail.com>
2025-05-17 16:32:46 +0200

    Update

--- 8Kyu/Avearage.js
+++ 8Kyu/Avearage.js
@@ -2,6 +2,9 @@ function findAverage(array) {
     console.log (array);
     let average;
     let sum = 0;
+    if (array.length === 0){
+        return 0;
+    }
     for(let i = 0 ; i < array.length; i++){
       sum = sum + array[i];
     }
